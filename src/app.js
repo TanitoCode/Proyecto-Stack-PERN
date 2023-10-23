224aaeea02c674125dba7fe59b177347f2ee2c93
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2023-10-22 22:28:24 -0300

    [ADD] Agrego controllers

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 import express from "express";
 import morgan from "morgan";
+import tareasRoutes from "./router/tareas.routes.js"
+import authRoutes from "./router/auth.routes.js"
 
 const app = express();
 
+//Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.json({ message: "Bienvenido a mi proyecto"}));
 
+app.use('/api',tareasRoutes);
+app.use('/api',authRoutes);
+
+
+//Manejando errores
 /*
 app.get('/test', (req, res) => {
 
---
+++ src/controllers/tareas.controller.js
@@ -0,0 +1,8 @@
+export const listarTareas = (req, res) => res.send('obteniendo tareas');
+export const listarTarea = (req, res) => res.send('Obteniendo tarea unica');
+
+export const crearTarea = (rew, res) => res.send('creando tarea');
+
+export const actualizarTarea = (req, res) => res.send('Actualizando tarea unica');
+
+export const eliminarTarea = (req, res) => res.send('Eliminando tarea unica');
