5c4547402496e21e0226bad5c279d64e056ea025
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2023-10-24 00:00:58 -0300

    [CGH]Metodo Listar tareas

--- src/controllers/tareas.controller.js
+++ src/controllers/tareas.controller.js
@@ -1,29 +1,37 @@
-import {pool} from '../db.js';
+import { pool } from '../db.js';
 
-export const listarTareas = (req, res) => res.send('obteniendo tareas');
-export const listarTarea = (req, res) => res.send('Obteniendo tarea unica');
+export const listarTareas = async (req, res) => {
 
-export const crearTarea = async(req, res, next) => {
-    const { titulo, descripcion } = req.body;
-    
-
-    try { 
-        throw new Error('Algo salio mal')
-        const {rows} = await pool.query('INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2)', [titulo, descripcion]);
-        console.log(rows);
-        res.send('creando tarea');
-    } catch (error){
-        if (error.code === '23505') {
-            return res.send('Ya existe una tarea con ese titulo');
-        }
-        console.log(error);
-        next(error);
-        
-    }
-
+    const resultado = await pool.query('SELECT * FROM tareas');
+    console.log(resultado);
+    return res.json(resultado.rows);
 
 }
+export const listarTarea = (req, res) => res.send('Obteniendo tarea unica');
 
-export const actualizarTarea = (req, res) => res.send('Actualizando tarea unica');
+export const crearTarea = async (req, res, next) => {
+    const { titulo, descripcion } = req.body;
 
-export const eliminarTarea = (req, res) => res.send('Eliminando tarea unica');
+
+    try {
+
+        const result = await pool.query('INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2) RETURNING *', [titulo, descripcion]);
+        res.json(result.rows[0]);
+        console.log(result.rows[0]);
+    } catch (error) {
+        if (error.code === '23505') {
+            return res.status(409).json({
+                mesagge: 'Ya existe una tarea con ese titulo'
+            });
+            console.log(error);
+            next(error);
+
+        }
+
+
+    }
+}
+
+    export const actualizarTarea = (req, res) => res.send('Actualizando tarea unica');
+
+    export const eliminarTarea = (req, res) => res.send('Eliminando tarea unica');
