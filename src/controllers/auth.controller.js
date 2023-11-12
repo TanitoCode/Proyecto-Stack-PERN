abdae306010912341fe5c47de3a1ba033ae2598c
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2023-11-11 22:17:26 -0300

    [ADD] Creacion del token- conectando con el frontend

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import {pool} from "../db.js"
 import bcrypt from "bcrypt";
+import {createAccessToken} from "../libs/jwt.js"
 
 export const signin = (req, res) => res.send("Ingresando");
 
@@ -7,15 +8,18 @@ export const signup = async(req, res) => {
     const {name, email, password} = req.body;
    
     
-
     try {
         const hashedPassword = await bcrypt.hash(password, 10)
         console.log(hashedPassword)
 
         const result = await pool.query("INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3) Returning *", [name, email, hashedPassword])
 
+        const token = await createAccessToken({id: result.rows[0].id})
         console.log(result);
-        return res.json(result.rows[0]);  
+        //return res.json(result.rows[0]);  
+        return res.json({
+            token: token,
+        });
 
     } catch (error){
         if(error.code === "23505"){
