77cc8621cf93b41f92de25c3deac752d24ec8898
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2023-11-11 21:53:36 -0300

    [CHG] Reparando errores

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,10 +4,20 @@ export const signin = (req, res) => res.send("Ingresando");
 
 export const signup = async(req, res) => {
     const {name, email, password} = req.body;
-    res.send("Registrando");
+   
+    
 
-    const result = await pool.query("INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3)", [name, email, password])
-    console.log(result);    
+    try {
+        const result = await pool.query("INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3) Returning *", [name, email, password])
+
+        console.log(result);
+        return res.json(result.rows[0]);  
+
+    } catch (error){
+        if(error.code === "23505"){
+            return res.status(400).json({message: "El correo ya esta registrado"});
+        }
+    }    
 };
 
 export const signout = (req, res) => res.send("Cerrando sesion");
