db4875faef10669fd2c1400cd685121834649ad8
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2023-11-12 12:51:27 -0300

    [ADD] Backend v1

--- database/init.sql
+++ database/init.sql
@@ -2,7 +2,10 @@ CREATE TABLE tareas (
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(255) UNIQUE NOT NULL,
     descripcion TEXT,
+
 );
+
+ALTER TABLE tareas ADD COLUNM usuario_id INTEGER REFERENCES usuarios(id);
 
 CREATE TABLE usuarios (
     id SERIAL PRIMARY KEY,
@@ -11,4 +14,8 @@ CREATE TABLE usuarios (
     password VARCHAR(255) NOT NULL,
     fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     fecha_actualizacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-)
+);  
+
+
+ALTER TABLE usuarios ADD COLUMN gravatar VARCHAR(255)
+
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import {pool} from "../db.js"
 import bcrypt from "bcrypt";
 import {createAccessToken} from "../libs/jwt.js"
+import md5 from "md5";
 
 export const signin = async (req, res) =>{
     const {email, password} = req.body;
@@ -30,10 +31,11 @@ export const signup = async(req, res, next) => {
    
     
     try {
-        const hashedPassword = await bcrypt.hash(password, 10)
-        console.log(hashedPassword)
+        const hashedPassword = await bcrypt.hash(password, 10);
+        md5(email);
+        const gravatar = "https://gravatar.com/avatar/" + md5(email);
 
-        const result = await pool.query("INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3) Returning *", [name, email, hashedPassword])
+        const result = await pool.query("INSERT INTO usuarios (name, email, password, gravatar) VALUES ($1, $2, $3, $4) Returning *", [name, email, hashedPassword, gravatar])
 
         const token = await createAccessToken({id: result.rows[0].id})
         console.log(result);
